72a99fca8bcb6489d1df4104d8af8946486bfd65
lokkju <lokkju@gmail.com>
2014-07-21 20:00:07 -0700

    removing old sub projects

--- gsmdecode/src/mcc_list.h
+++
@@ -1,5 +0,0 @@
-
-
-const char *mcc_get(int mcc);
-const char *mnc_get(int mcc, int mnc);
-
--- gssm/src/lib/buffer.h
+++
@@ -1,4 +0,0 @@
-#pragma once
-
-static const int BUFFER_QD_SIZE =	16384;
-static const int BUFFER_MM_SIZE =	16384;
